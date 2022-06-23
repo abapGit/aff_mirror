f2e0706d675e3450ad335042b82d6576afbfba8c
Albert Mink <albert.mink@sap.com>
2022-06-23 09:34:13 +0200

    Fix clsname in XML (#34)

--- add-xml.sh
+++ add-xml.sh
@@ -7,12 +7,14 @@ done
 
 
 for x in src/*.xml; do
+  clsname=$(basename "$x" .intf.xml)
+  clsnameUPPER=`echo $clsname | tr a-z A-Z`
   echo '<?xml version="1.0" encoding="utf-8"?>' > $x
   echo '<abapGit version="v1.0.0" serializer="LCL_OBJECT_INTF" serializer_version="v1.0.0">' >> $x
   echo ' <asx:abap xmlns:asx="http://www.sap.com/abapxml" version="1.0">' >> $x
   echo '  <asx:values>' >> $x
   echo '   <VSEOINTERF>' >> $x
-  echo '    <CLSNAME>'${x%%.*}'</CLSNAME>' >> $x
+  echo '    <CLSNAME>'$clsnameUPPER'</CLSNAME>' >> $x
   echo '    <LANGU>E</LANGU>' >> $x
   echo '    <DESCRIPT>ABAP file format types</DESCRIPT>' >> $x
   echo '    <EXPOSURE>2</EXPOSURE>' >> $x
